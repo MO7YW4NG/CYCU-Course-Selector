c06dacda330ff89286556ef272a07bc3952a2898
MO7YW4NG <motywang@gmail.com>
2023-09-17 01:21:26 +0800

    Update - Version upate

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ target = Executable(
 
 setup(
     name="CYCU Course Selector",
-    version="1.0",
+    version="1.1",
     description="CYCU course selector app",
     author="MO7YW4NG",
     executables=[target],
