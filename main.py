957683d94e477b88fe2e73106146d1fe47acbba1
MO7YW4NG <motywang@gmail.com>
2023-09-20 22:37:24 +0800

    Code improvements

--- main.py
+++ main.py
@@ -5,74 +5,70 @@ import os
 import getpass
 
 elecAuthData = AuthData()
-loginToken = ""
-token = ""
-cookies = ""
-info = LoginInfo("default","default")
 
-def elecAuth():
-    global cookies
-    res = req(Direct["AUTH"],elecAuthData,None,cookies)
+def login(login_info):
+    res = loginReq(Direct["LOGIN"], login_info)
+    if res.status_code != 200:
+        print("請求傳送失敗! 請檢查網路連線!")
+        return None, None
+    if res.text == "伺服器執行錯誤(i)":
+        print("登入失敗! 請檢查學號及密碼是否正確!")
+        return None, None
+    msg = res.json()
+    if msg['done_YN'] == "N":
+        print("登入失敗! 請檢查學號及密碼是否正確!")
+        return None, None
+    return msg['loginToken'], res.cookies
+
+def auth(login_token, login_info, cookies):
+    res = req(Direct["AUTH"], elecAuthData, None, cookies)
     msg = res.json()
     if "APP_AUTH_token" not in msg:
-        global loginToken
-        lgReq = req(Direct["LOGIN"],info,None,cookies)
-        loginToken = lgReq.json()["loginToken"]
-        cookies = lgReq.cookies
+        login(login_info)
         sleep(1)
-        return elecAuth()
-    global token
-    token = msg["APP_AUTH_token"]
+        return auth(login_token, login_info, cookies)
+    return msg["APP_AUTH_token"], cookies
 
-def task():
-    while(True):
-        reqInfo = RequestInfo(token)
-        paramInfo = ParamInfo(loginToken,"track_get")
-        trackReq = req(Direct["ELEC"],reqInfo,paramInfo,cookies)
-        if trackReq.status_code != 200:
-            elecAuth()
+def request_course(login_token, auth_token, cookies):
+    req_info = RequestInfo(auth_token)
+    param_info = ParamInfo(login_token, "track_get")
+    track_req = req(Direct["ELEC"], req_info, param_info, cookies)
+    if track_req.status_code != 200:
+        return track_req.status_code
+    msg = track_req.json()
+    for item in msg["track_get"]:
+        insert_info = InsertInfo([{"DATA_Token": item["DATA_Token"]}])
+        insert_req = req(Direct["ELEC"], req_info.insert_info(insert_info), param_info.set_mtd("take_course_and_register_insert"),cookies)
+        if insert_req.status_code != 200:
             continue
-        msg = trackReq.json()
-        for item in msg["track_get"]:
-            insertInfo = InsertInfo([{"DATA_Token":item["DATA_Token"]}])
-            insertReq = req(Direct["ELEC"],reqInfo.insert_info(insertInfo),paramInfo.set_mtd("take_course_and_register_insert"),cookies)
-            if insertReq.status_code != 200:
-                continue
-            newMsg = insertReq.json()
-            if "distinct_IP_IDCODE_alert" in newMsg:
-                req(Direct["ELEC"],reqInfo,paramInfo.method("login_sys_upd"),cookies)
-                break
-            if "alert_text" in newMsg:
-                print("[" + item["CURS_CODE"] + "] " + item["CNAME"] + " [❎]: " + newMsg["alert_text"])
-            else:
-                print("[" + item["CURS_CODE"] + "] " + item["CNAME"] + " [✅]: " + newMsg["alerts"])
-    os.system("pause")
+        new_msg = insert_req.json()
+        if "distinct_IP_IDCODE_alert" in new_msg:
+            req(Direct["ELEC"], req_info, param_info.method("login_sys_upd"), cookies)
+            break
+        if "alert_text" in new_msg:
+            print("[" + item["CURS_CODE"] + "] " + item["CNAME"] + " [❎]: " + new_msg["alert_text"])
+        else:
+            print("[" + item["CURS_CODE"] + "] " + item["CNAME"] + " [✅]: " + new_msg["alerts"])
+    return track_req.status_code
 
-if __name__ == '__main__':
+def main():
     os.system("title 搶課機器人")
     try:
+        id = input("輸入您的學號：")
+        pwd = getpass.getpass("輸入您的itouch密碼：")
+        login_info = LoginInfo(id, pwd)
+        login_token, cookies = login(login_info)
+        if login_token is None:
+            return main()
+        print("登入成功! 開始搶課!")
+        auth_token, cookies = auth(login_token, login_info, cookies)
+        sleep(0.1)
         while(True):
-            id = input("輸入您的學號：")
-            passwd = getpass.getpass("輸入您的itouch密碼：")
-            info = LoginInfo(id,passwd)
-            res = loginReq(Direct["LOGIN"],info)
-            if res.status_code != 200:
-                print("請求傳送失敗!")
-                continue
-            if res.text == "伺服器執行錯誤(i)":
-                print("登入失敗! 請檢查學號及密碼是否正確!")
-                continue
-            msg = res.json()
-            if msg['done_YN'] == "N":
-                print("登入失敗! 請檢查學號及密碼是否正確!")
-                continue
-            print("登入成功! 開始搶課!")
-            cookies = res.cookies
-            loginToken=msg['loginToken']
-            elecAuth()
-            sleep(0.1)
-            task()
-            break
+            if request_course(login_token, auth_token, cookies) != 200:
+                auth_token, cookies = auth(login_token, login_info, cookies)
     except Exception as e:
         print(e)
         os.system("pause")
+
+if __name__ == '__main__':
+    main()
