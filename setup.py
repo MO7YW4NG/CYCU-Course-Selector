602a5d0e90fd4ef3462495d468f6c52ad095dade
MO7YW4NG <motywang@gmail.com>
2024-04-25 03:13:45 +0800

    Updated version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ target = Executable(
 
 setup(
     name="CYCU Course Selector",
-    version="1.3",
+    version="1.4",
     description="CYCU course selector app",
     author="MO7YW4NG",
     options={'bdist_msi': {'initial_target_dir': r'[DesktopFolder]\\CYCU Course Selector'}},
